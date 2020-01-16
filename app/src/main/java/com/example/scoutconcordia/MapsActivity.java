0d16df74eac70d6c4c805b594a7e30b84b0f15e6
Jay Chen <jaychenblue@gmail.com>
2020-01-15 21:15:21 -0500

    Add comments

--- app/src/main/java/com/example/scoutconcordia/MapsActivity.java
+++ app/src/main/java/com/example/scoutconcordia/MapsActivity.java
@@ -30,7 +30,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
      * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
+     * we just add a marker near Concordia (downtown campus).
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
@@ -39,12 +39,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        // LatLng sydney = new LatLng(-34, 151);
+        // Add a marker in Concordia and move the camera
         LatLng coco = new LatLng(45.494619, -73.577376); // Concordia's coordinates
         mMap.addMarker(new MarkerOptions().position(coco).title("Marker in Concordia"));
         float zoomLevel = 16.0f; // max 21
-        // mMap.moveCamera(CameraUpdateFactory.newLatLng(coco));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(coco, zoomLevel));
     }
 }
