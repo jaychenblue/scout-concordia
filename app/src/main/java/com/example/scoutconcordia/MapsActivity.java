21a78adb2297d6403f78b7d8304cda93fe77117b
mano0U7 <sohal001@hotmail.com>
2020-01-16 22:06:14 -0500

    map current location

--- app/src/main/java/com/example/scoutconcordia/MapsActivity.java
+++ app/src/main/java/com/example/scoutconcordia/MapsActivity.java
@@ -1,48 +1,98 @@
 package com.example.scoutconcordia;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
-
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
-
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private float zoomLevel = 16.0f;
+    private final int ACCESS_FINE_LOCATION = 9001;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    final private LatLng concordiaLatLngDowntownCampus = new LatLng(45.494619, -73.577376);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Concordia (downtown campus).
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Add a marker in Concordia and move the camera
-        LatLng coco = new LatLng(45.494619, -73.577376); // Concordia's coordinates
-        mMap.addMarker(new MarkerOptions().position(coco).title("Marker in Concordia"));
-        float zoomLevel = 16.0f; // max 21
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(coco, zoomLevel));
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            getCurrentLocation();
+        }
+        else{
+            // request for user's permission for location services
+            animateCamera(concordiaLatLngDowntownCampus, zoomLevel);
+            requestPermissions();
+        }
     }
+
+    private void animateCamera(LatLng latLng, float zoomLevel){
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoomLevel));
+    }
+
+    private void getCurrentLocation(){
+        mMap.setMyLocationEnabled(true);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        try{
+            Task location = fusedLocationProviderClient.getLastLocation();
+            location.addOnCompleteListener(new OnCompleteListener() {
+                @Override
+                public void onComplete(@NonNull Task task) {
+                    if(task.isSuccessful()){
+                        Location currentLocation = (Location) task.getResult();
+                        LatLng currentLatLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+                        animateCamera(currentLatLng, zoomLevel);
+                    }
+                }
+            });
+        }catch (SecurityException e){
+            // some problem occurred, return Concordia downtown Campus Location
+            animateCamera(concordiaLatLngDowntownCampus, zoomLevel);
+        }
+    }
+
+    private void requestPermissions(){
+        // permission not granted, request for permission
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, ACCESS_FINE_LOCATION);
+        } else {
+            // Permission has already been granted, DO NOTHING
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case ACCESS_FINE_LOCATION: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission granted, move the camera to current location
+                    getCurrentLocation();
+                }
+            }
+        }
+    }
+
 }
