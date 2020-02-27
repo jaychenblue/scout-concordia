e0f4a3eac3816bdb55549259f1ce8e655c3bd5d6
Victor Manea <36647329+vicmanea@users.noreply.github.com>
2020-02-27 16:21:50 -0500

    This adds the updated and color-specific functionality of a Shuttle Bus Schedule page when clicking on the button.

--- app/src/main/java/com/example/scoutconcordia/ShuttleScheduleDisplayActivity.java
+++ app/src/main/java/com/example/scoutconcordia/ShuttleScheduleDisplayActivity.java
@@ -2,6 +2,7 @@ package com.example.scoutconcordia;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ShuttleScheduleDisplayActivity extends AppCompatActivity {
@@ -9,6 +10,9 @@ public class ShuttleScheduleDisplayActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_shuttle_schedule_display);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
     }
 }
