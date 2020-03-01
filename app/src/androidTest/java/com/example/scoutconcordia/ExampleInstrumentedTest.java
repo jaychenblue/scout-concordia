1b932054c22cdd239e072405225cdc16cf7af8b1
jaychenblue <46407459+jaychenblue@users.noreply.github.com>
2020-02-29 22:05:49 -0500

    Revert "Testing"

--- app/src/androidTest/java/com/example/scoutconcordia/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/scoutconcordia/ExampleInstrumentedTest.java
@@ -4,17 +4,11 @@ import android.content.Context;
 
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.rule.ActivityTestRule;
 
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import static org.junit.Assert.*;
 
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.*;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -23,20 +17,11 @@ import static androidx.test.espresso.matcher.ViewMatchers.withText;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-    /*
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
         assertEquals("com.example.scoutconcordia", appContext.getPackageName());
-    }*/
-    @Rule
-    public ActivityTestRule<MainActivity> activityRule =
-            new ActivityTestRule<>(MainActivity.class);
-
-    @Test
-    public void listGoesOverTheFold() {
-        onView(withText("Hello world!")).check(matches(isDisplayed()));
     }
 }
--- app/src/test/java/com/example/scoutconcordia/ExampleUnitTest.java
+++ app/src/test/java/com/example/scoutconcordia/ExampleUnitTest.java
@@ -1,14 +1,17 @@
 package com.example.scoutconcordia;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
 public class ExampleUnitTest {
-
     @Test
-    public void testThis() {
+    public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
-
 }
