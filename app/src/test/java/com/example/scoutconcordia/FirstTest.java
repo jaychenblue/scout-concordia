d8a6a5d129b634ae3183d412174b27b40d327c83
Jay Chen <jaychenblue@gmail.com>
2020-03-08 21:50:02 -0400

    Write first sample test

---
+++ app/src/test/java/com/example/scoutconcordia/FirstTest.java
@@ -0,0 +1,14 @@
+package com.example.scoutconcordia;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FirstTest {
+
+    @Test
+    public void firstTest() {
+        assertEquals(5, 2+3);
+    }
+
+}
