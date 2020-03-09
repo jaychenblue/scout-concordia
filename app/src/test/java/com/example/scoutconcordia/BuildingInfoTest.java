b54f84954a670115962ff3ba2fc39d3cbb92c6da
Jay Chen <jaychenblue@gmail.com>
2020-03-08 22:14:36 -0400

    Add setup and teardown functions to BuildingInfoTest

--- app/src/test/java/com/example/scoutconcordia/BuildingInfoTest.java
+++ app/src/test/java/com/example/scoutconcordia/BuildingInfoTest.java
@@ -1,18 +1,32 @@
 package com.example.scoutconcordia;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class BuildingInfoTest {
 
+    BuildingInfo b;
+
+    @Before
+    public void before() throws Exception {
+        b = new BuildingInfo("ABC", "123", "0800");
+    }
+
     @Test
     public void getNameTest() {
-        BuildingInfo b = new BuildingInfo("ABC", "123", "0800");
-
         String output = b.getName();
 
         assertEquals("ABC", output);
+    }
+
+    @After
+    public void after() throws Exception {
+        b = null;
+        assertNull(b);
     }
 
 }
