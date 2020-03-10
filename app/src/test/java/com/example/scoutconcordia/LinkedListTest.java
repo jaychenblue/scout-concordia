d12e57b52ff5a74bab5724148d96754d36e8aa12
Jay Chen <jaychenblue@gmail.com>
2020-03-10 12:40:03 -0400

    [#145] Write all unit tests corresponding to LinkedList

--- app/src/test/java/com/example/scoutconcordia/LinkedListTest.java
+++ app/src/test/java/com/example/scoutconcordia/LinkedListTest.java
@@ -3,24 +3,30 @@ package com.example.scoutconcordia;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 import java.lang.Integer;
 
 public class LinkedListTest {
 
     LinkedList<Integer> l;
+    Integer[] numbs;
 
     @Before
     public void before() throws Exception {
-        Integer number = new Integer(1);
+        numbs = new Integer[]{1, 2, 3};
 
-        l = new LinkedList<Integer>(number);
+        l = new LinkedList<Integer>(1, numbs);
     }
 
     @After
     public void after() throws Exception {
+        numbs = null;
         l = null;
         assertNull(l);
     }
@@ -28,75 +34,54 @@ public class LinkedListTest {
     // test constructors
     @Test
     public void testStandardConstructor() {
+        LinkedList<Integer> list = new LinkedList<Integer>(1);
 
+        assertNull(list.getHead());
+        assertEquals(0, list.size());
+
+        list = null;
     }
 
     @Test
     public void testCustomConstructor() {
+        Integer[] arr = new Integer[]{5, 7, 9, 11, 13000000, -999999};
+        LinkedList<Integer> list = new LinkedList<Integer>(1, arr);
 
+        assertNotNull(list);
+        assertEquals(6, list.size());
     }
 
     // test getters
     @Test
     public void testGetters() {
-
-    }
-
-    // test adding stuff
-    @Test
-    public void testAddToHead() {
-
-    }
-
-    @Test
-    public void testAddToTail() {
-
-    }
-
-    @Test
-    public void testAddToIndex() {
-
-    }
-
-    // test nodes
-    @Test
-    public void testGetNodeFromIndex() {
-
-    }
-
-    @Test
-    public void testRemoveNode() {
-
-    }
-
-    // test remove
-    @Test
-    public void testRemove() {
-
+        assertEquals(3, l.size());
+        assertNotNull(l.getHead());
+        assertNotNull(l.remove(1));
+        assertTrue(l.add(77));
     }
 
     // test clear
     @Test
     public void testClear() {
-
+        l.clear();
+        assertEquals(0, l.size());
+        assertNotNull(l);
     }
 
     // test toString
     @Test
     public void testToString() {
-
-    }
-
-    // test unsupported operations
-    @Test
-    public void testUnsupportedOperations() {
-
+        assertEquals("Size: 3\n" +
+                "Entry 0: 1\n" +
+                "Entry 1: 2\n" +
+                "Entry 2: 3\n",
+                l.toString());
     }
 
     // test remove object
     @Test
     public void testRemoveOBJ() {
-
+        assertTrue(l.removeOBJ(2));
     }
 
 }
