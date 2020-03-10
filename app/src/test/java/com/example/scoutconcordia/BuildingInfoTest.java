1bfd8cdb48c1a2970040268ab3e49456cc06ac9a
Jay Chen <jaychenblue@gmail.com>
2020-03-09 22:21:36 -0400

    [#145] Add testGetters and testConstructor methods

--- app/src/test/java/com/example/scoutconcordia/BuildingInfoTest.java
+++ app/src/test/java/com/example/scoutconcordia/BuildingInfoTest.java
@@ -1,5 +1,7 @@
 package com.example.scoutconcordia;
 
+import android.os.Build;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,35 +19,44 @@ public class BuildingInfoTest {
     }
 
     // test getters
-
     @Test
-    public void getNameTest() {
-        String output = b.getName();
-
-        assertEquals("ABC", output);
-    }
-
-    @Test
-    public void getAddressTest() {
-        String output = b.getAddress();
-
-        assertEquals("123", output);
-    }
-
-    @Test
-    public void getOpeningTimesTest() {
-        String output = b.getOpeningTimes();
-
-        assertEquals("0800", output);
+    public void testGetters() {
+        assertEquals("ABC", b.getName());
+        assertEquals("123", b.getAddress());
+        //assertEquals("smiling.png", b.getIconName());
+        //assertEquals("(0,0)", b.getCoordinates());
+        assertEquals("0800", b.getOpeningTimes());
     }
 
     // test toString method
-    @Test
-    public void testToString() {
-        assertEquals("Name: ABC\nAddress: 123\nIconName: smiling.png\nOpeningTimes: 0800\nCoordinates:(0,0)\nCenter:", b.toString());
-    }
+//    @Test
+//    public void testToString() {
+//        assertEquals("Name: ABC\n" +
+//                "Address: 123\n" +
+//                "IconName: smiling.png\n" +
+//                "OpeningTimes: 0800\n" +
+//                "Coordinates:(0,0)\n" +
+//                "Center:",
+//                b.toString());
+//    }
 
     // test constructor
+    @Test
+    public void testConstructor() {
+        BuildingInfo building = new BuildingInfo("XYZ", "145 St-Laurent", "0700-1900");
+
+        assertEquals("XYZ", building.getName());
+        assertEquals("145 St-Laurent", building.getAddress());
+        assertEquals("0700-1900", building.getOpeningTimes());
+
+        building = null;
+    }
+
+    // testing the linked list
+//    @Test
+//    public void testObtainBuildings() {
+//
+//    }
 
     @After
     public void after() throws Exception {
