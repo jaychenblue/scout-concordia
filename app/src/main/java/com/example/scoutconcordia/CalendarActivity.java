e2855225ce52a9d99dbd2307b5eb8a8583026ae3
mano0U7 <sohal001@hotmail.com>
2020-03-14 03:31:25 -0400

    [#7] new implementation of the google sign in feature after it was corrupted by the conflicts that arose during merging and had to be reomved

--- app/src/main/java/com/example/scoutconcordia/CalendarActivity.java
+++ app/src/main/java/com/example/scoutconcordia/CalendarActivity.java
@@ -2,19 +2,40 @@ package com.example.scoutconcordia;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import static android.content.ContentValues.TAG;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class CalendarActivity extends AppCompatActivity {
+    private GoogleSignInClient googleSignInClient;
+    private GoogleSignInOptions gso;
+    private static final int RC_SIGN_IN = 9001; // request code for the signIn intent (onActivityResult request code)
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestScopes(new Scope("https://www.googleapis.com/auth/calendar.readonly")) // requesting google calendar scope
+                .requestEmail()
+                .build();
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso);
+        signIn();
 
         BottomNavigationView bottomNavigationView = (BottomNavigationView)findViewById(R.id.nav_bar_activity_calendar);
         bottomNavigationView.setSelectedItemId(R.id.nav_schedule);
@@ -40,6 +61,35 @@ public class CalendarActivity extends AppCompatActivity {
                 return false;
             }
         });
+    }
+
+    // creates a new sign in intent
+    protected void signIn() {
+        Intent signInIntent = googleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    // result of the starActivityForResult for the singInIntent in signIn()
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // if the request code equals that of the signInIntent
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInresult(task);
+        }
+    }
+
+    // handles the behaviour once the sign in has completed
+    protected void handleSignInresult(Task<GoogleSignInAccount> task) {
+        try {
+            GoogleSignInAccount account = task.getResult(ApiException.class);
+            Toast.makeText(getApplicationContext(),account.getEmail(), Toast.LENGTH_LONG).show();
+        } catch (Throwable e) {
+            Toast.makeText(getApplicationContext(),e.toString(), Toast.LENGTH_LONG).show();
+            Log.d(TAG, e.getMessage());
+        }
     }
 
     @Override
