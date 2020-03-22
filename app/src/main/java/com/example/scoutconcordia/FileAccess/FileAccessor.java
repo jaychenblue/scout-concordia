f4fe0f328944d604512d91465cc4aa22dedeb225
Averynder <averyndersingh@gmail.com>
2020-03-22 19:27:54 -0400

    [#183] Made fileName as Data member
    The purpose of this is so that the user / dev can set the file name to whatever they want, decrypt the file, read from it and close it without having to specify the filename for all 3 methods. This will make it easier to loop.

--- app/src/main/java/com/example/scoutconcordia/FileAccess/FileAccessor.java
+++ app/src/main/java/com/example/scoutconcordia/FileAccess/FileAccessor.java
@@ -13,13 +13,21 @@ import java.io.OutputStream;
 public class FileAccessor
 {
     DES encryptionAccesser;
+    String fileName;
     
     public FileAccessor()
     {
         encryptionAccesser = new DES();
+        fileName = null;
     }
     
-    public void decryptFile(String fileName)
+    public void setFileName(String fileName)
+    {
+        this.fileName = fileName;
+    }
+    
+    // Used first to be able to read the file
+    public void decryptFile()
     {
         InputStream readme = null;
         OutputStream writeToMe = null;
@@ -28,7 +36,6 @@ public class FileAccessor
             readme = encryptionAccesser.getResources().openRawResource(
             encryptionAccesser.getResources().getIdentifier(fileName, "raw", encryptionAccesser.getPackageName()));
             writeToMe = new FileOutputStream(new File(encryptionAccesser.getFilesDir().getAbsoluteFile(), fileName));
-            //DES.encryptFile(readme, writeToMe);
             DES.decryptFile(readme, writeToMe);
         }
         catch (FileNotFoundException fnf)
@@ -39,5 +46,12 @@ public class FileAccessor
         {
             Log.println(Log.WARN, "FileAccessor", "The input file could not be located");
         }
+    }
+    
+    // used when done to make sure no information is potentially leaked
+    public boolean closeFile()
+    {
+        File deleteMe = new File(encryptionAccesser.getFilesDir().getAbsoluteFile(), fileName);
+        return deleteMe.delete();
     }
 }
