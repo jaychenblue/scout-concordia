737313c0c19edf555470c6970cc3cc93966c8521
Jay <jaychenblue@gmail.com>
2020-03-28 14:36:09 -0400

    [#186] Updated GraphTest to include a new parameter in insertVertex()
    The new parameter in insertVertex() was breaking all the tests so I updated them accordingly; the basic skeleton for ShuttleInfoTest was also added

---
+++ app/src/test/java/com/example/scoutconcordia/ShuttleInfoTest.java
@@ -0,0 +1,80 @@
+package com.example.scoutconcordia;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ShuttleInfoTest {
+
+    @BeforeEach
+    public void beforeEach() throws Exception {
+
+    }
+
+    @AfterEach
+    public void afterEach() throws Exception {
+
+    }
+
+    // checking calculated route times
+
+    @Test
+    public void testGetEstimatedRouteTimeFromSGW() {
+
+    }
+
+    @Test
+    public void testGetEstimatedRouteTimeFromLoyola() {
+
+    }
+
+    // checking shuttle times
+
+    @Test
+    public void testGetLoyolaMondayToThursdayTimes() {
+
+    }
+
+    @Test
+    public void testGetSGWMondayToThursdayTimes() {
+
+    }
+
+    @Test
+    public void testGetLoyolaFridayTimes() {
+
+    }
+
+    @Test
+    public void testGetSGWFridayTimes() {
+
+    }
+
+    // checking retrieval of times from encrypted files
+
+    @Test
+    public void testRetrieveMonToThursLoyola() {
+
+    }
+
+    @Test
+    public void testRetrieveFridayLoyola() {
+
+    }
+
+    @Test
+    public void testRetrieveMonToThursSGW() {
+
+    }
+
+    @Test
+    public void testRetrieveFridaySGW() {
+
+    }
+
+}
