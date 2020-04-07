ca8c065a3965497ef5f58278cc757da652e6270c
JenYn <jyounanian@gmail.com>
2020-04-07 13:39:45 -0400

    [#119] Menu now appears on the shuttle schedule page. Will need to refactor. Only thing missing to the menu is the language toggle and accessibility toggle.

--- app/src/main/java/com/example/scoutconcordia/Activities/ShuttleScheduleActivity.java
+++ app/src/main/java/com/example/scoutconcordia/Activities/ShuttleScheduleActivity.java
@@ -2,9 +2,12 @@ package com.example.scoutconcordia.Activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.example.scoutconcordia.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -15,6 +18,10 @@ public class ShuttleScheduleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shuttle_schedule);
+
+        //Toolbar on top of the page
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
 
         BottomNavigationView bottomNavigationView = (BottomNavigationView)findViewById(R.id.nav_bar_activity_shuttle_schedule);
         bottomNavigationView.setSelectedItemId(R.id.nav_shuttle);
@@ -46,5 +53,36 @@ public class ShuttleScheduleActivity extends AppCompatActivity {
     public void onBackPressed() {
         super.onBackPressed();
         overridePendingTransition(0, 0);
+    }
+
+    //inflates the menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    //Handling menu clicks
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem menuItem) {
+        // Handle item selection
+        switch (menuItem.getItemId()) {
+            case R.id.main_home:
+                Intent mapIntent = new Intent(ShuttleScheduleActivity.this, MapsActivity.class);
+                startActivity(mapIntent);
+                ShuttleScheduleActivity.this.overridePendingTransition(0, 0);
+                break;
+
+            case R.id.main_schedule:
+                Intent calendarIntent = new Intent(ShuttleScheduleActivity.this, CalendarActivity.class);
+                startActivity(calendarIntent);
+                ShuttleScheduleActivity.this.overridePendingTransition(0, 0);
+                break;
+            case R.id.main_shuttle:
+               break;
+
+        }
+        return false;
     }
 }
