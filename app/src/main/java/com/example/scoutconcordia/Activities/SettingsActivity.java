10d5847590d1ac54786051dd226baa66724e5611
JenYn <jyounanian@gmail.com>
2020-04-08 18:36:58 -0400

    [#119] Added settings activity.

---
+++ app/src/main/java/com/example/scoutconcordia/Activities/SettingsActivity.java
@@ -0,0 +1,16 @@
+package com.example.scoutconcordia.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.scoutconcordia.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
