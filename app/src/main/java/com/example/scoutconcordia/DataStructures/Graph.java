5518bfd9712aa2b66d42080f3febfe927f418533
Vincent <vincent.cerri@gmail.com>
2020-04-13 17:05:11 -0400

    [#258] Added Javadoc comments to the Graph class

--- app/src/main/java/com/example/scoutconcordia/DataStructures/Graph.java
+++ app/src/main/java/com/example/scoutconcordia/DataStructures/Graph.java
@@ -1,26 +1,21 @@
 package com.example.scoutconcordia.DataStructures;
 
 import android.util.Log;
-
 import com.google.android.gms.maps.model.LatLng;
-
-import java.io.InputStream;
 import java.util.InputMismatchException;
-import java.util.Scanner;
-
-import io.opencensus.trace.Link;
-
 import static android.location.Location.distanceBetween;
 
-
-// Assumption is no two points are equivalent
+/** Class representing a Graph.*/
 public class Graph
 {
     private int numberOfNodes;
     private Node[] nodes;
     private N_aryTree breathFirstSearchResults;
-    public String id; //will contain the name of the floor
+    /** Represents the name of the floor that the Graph is being used for
+     */
+    public String id;
 
+    /** Inner class representing a Node of the Graph. */
     public class Node
     {
         private int id;
@@ -30,6 +25,12 @@ public class Graph
         private int type;  // 0 is a class node, 1 is a hall node.
         private String room; //this is the corresponding room name for the node
 
+        /**Creates a Node with a specified location
+         * @param element The latitude and longitude coordinate of the node
+         * @param id Index of the node in the Node array from the Graph.
+         * @param type Integer representing the type of node. 0 = class node. 1 is a hallway node.
+         * @param room The corresponding room name for the node.
+         */
         public Node(LatLng element, int id, int type, String room)
         {
             this.element = element;
@@ -52,14 +53,17 @@ public class Graph
 
         private int getId() { return id; }
         private void setId(int id) { this.id = id; }
-        public int getType() { return type; }
-        public LatLng getElement() { return element; }
-        public String getRoom() {return room;}
         private void setElement(LatLng element) { this.element = element; }
         private boolean isTraversed() { return traversed; }
         private void setTraversed(boolean traversed) { this.traversed = traversed; }
+        public int getType() { return type; }
+        public LatLng getElement() { return element; }
+        public String getRoom() {return room;}
     }
 
+    /** Creates a Graph with a specified size.
+     * @param size The size of the graph to be created.
+     */
     public Graph(int size)
     {
         numberOfNodes = 0;
@@ -69,7 +73,8 @@ public class Graph
         breathFirstSearchResults = new N_aryTree();
     }
 
-    // resets the graph to start from fresh
+    /** Resets the graph to start from fresh.
+     */
     public void reset()
     {
         nodes = null;
@@ -77,7 +82,11 @@ public class Graph
         breathFirstSearchResults = null;
     }
 
-    // check the placement in the array of the given point / node
+    /**
+     * Check the placement in the array of the given point / node
+     * @param ele latitude and longitude coordinate of the node to check
+     * @return Returns an integer representing the index of the node in the array of nodes.
+     */
     private int getID(LatLng ele)
     {
         int id = -1;
@@ -90,8 +99,12 @@ public class Graph
         return id;
     }
 
-    // returns true if element can be inserted false otherwise
-    // type indicates whether the node is a class room node or a hall node
+    /** Inserts a new node into the Graph
+     * @param element The latitude and longitude coordinate of the node
+     * @param type Integer representing the type of node. 0 = class node. 1 is a hallway node.
+     * @param room The corresponding room name for the node.
+     * @return A Boolean signaling if the operation was successful.
+     */
     public boolean insertVertex(LatLng element, int type, String room)
     {
         int placeMe = -1;
@@ -108,7 +121,13 @@ public class Graph
         return true;
     }
 
-    // returns 1 if successful, 0 if the edge already exists, -1 if point not in graph
+    /** Inserts an edge between 2 vertices (nodes) in the graph.
+     * @param ele1 The latitude and longitude coordinate of the 1st node.
+     * @param ele2 The latitude and longitude coordinate of the 2nd node.
+     * @return An integer signaling if the operation was successful. -1 means 1 or both of the
+     * nodes are not in the graph. 0 means that these 2 vertices are already adjacent. 1 means that
+     * the operation was successful and the vertices are now adjacent.
+     */
     public int insertEdge(LatLng ele1, LatLng ele2)
     {
         int id1 = getID(ele1), id2 = getID(ele2);
@@ -121,18 +140,24 @@ public class Graph
         return 1;
     }
 
-    // returns true if adjacent, false if not
+    /** Checks to see if 2 nodes are adjacent to each other.
+     * @param ele1 The latitude and longitude coordinate of the 1st node.
+     * @param ele2 The latitude and longitude coordinate of the 2nd node.
+     * @return A boolean signaling if the 2 nodes are adjacent to each other.
+     */
     public boolean areAdjacent(LatLng ele1, LatLng ele2)
     {
         int id1 = getID(ele1), id2 = getID(ele2);
         if (id1 < 0 || id2 < 0)
             return false;
-        if (nodes[id1].adjacencyList.contains(nodes[id2]))
-            return true;
-        return false;
+        return nodes[id1].adjacencyList.contains(nodes[id2]);
     }
 
-    // replaces a coordinate in the graph with a new one
+    /** Replaces a coordinate in the graph with a new one
+     * @param oldCoordinate The latitude and longitude coordinate of the old node.
+     * @param newCoordinate The latitude and longitude coordinate of the new node.
+     * @return A boolean signaling if the operation was successful.
+     */
     public boolean replace(LatLng oldCoordinate, LatLng newCoordinate)
     {
         int id = getID(oldCoordinate);
@@ -143,6 +168,14 @@ public class Graph
     }
 
     // returns -1 if the elements can't be found in the nodes of the graph, 0 if one of the elements couldn't be removed / wasn't in the adjacency list and 1 if it was successful
+
+    /** Removes an edge between 2 vertices (nodes)
+     * @param ele1 The latitude and longitude coordinate of the 1st node.
+     * @param ele2 The latitude and longitude coordinate of the 2nd node.
+     * @return An integer signaling if the operation was successful. -1 means 1 or both of the
+     * nodes are not in the graph. 0 if one of the elements couldn't be removed / wasn't in the
+     * adjacency list. 1 if the operation was successful and the edge has been removed.
+     */
     public int removeEdge(LatLng ele1, LatLng ele2)
     {
         int id1 = getID(ele1), id2 = getID(ele2);
@@ -158,7 +191,11 @@ public class Graph
         return 1;
     }
 
-    // returns -1 if element isn't in the graph, 0 if the element to be removed wasn't in one of it's own adjacent entries / system error and 1 if successful
+    /** Removes a vertex from the Graph.
+     * @param removeMe The latitude and longitude coordinate of the node to be removed from the Graph.
+     * @return An integer signaling if the operation was successful. -1 if the node is not in the graph.
+     *  0 if the element to be removed wasn't in one of it's own adjacent entries. 1 if successful.
+     */
     public int removeVertex(LatLng removeMe)
     {
         int id = getID(removeMe);
@@ -175,7 +212,10 @@ public class Graph
         return 1;
     }
 
-    // returns any vertices next to the coordinate
+    /** Returns any vertices next to a specified coordinate.
+     * @param coordinate The latitude and longitude coordinate of the node to be used.
+     * @return A LatLng array containing all coordinates that are incident to the original node.
+     */
     public LatLng[] incidentVerticies(LatLng coordinate)
     {
         int id = getID(coordinate);
@@ -188,56 +228,67 @@ public class Graph
         return returnMe;
     }
 
-    // returns an array of all the elements in the graph
+    /**
+     * @return Returns an array of all the elements in the graph.
+     */
     public LatLng[] vertices()
     {
-        int getMe = -1, placeMe = 0;
+        int placeMe = 0;
         LatLng[] returnMe = new LatLng[numberOfNodes];
-        for (int i = 0; i < nodes.length; i++)
+        for (Node node : nodes)
         {
-            if (nodes[i] != null)
+            if (node != null)
             {
-                returnMe[placeMe] = new LatLng(nodes[i].getElement().latitude, nodes[i].getElement().longitude);
+                returnMe[placeMe] = new LatLng(node.getElement().latitude, node.getElement().longitude);
                 placeMe++;
             }
         }
         return returnMe;
     }
 
-    // returns an array of all the nodes in the graph
+    /**
+     * @return Returns an array of all the nodes in the graph.
+     */
     public Node[] nodes()
     {
         return nodes;
     }
 
+    /** Searches the array of nodes for a specific class node using the name of the class.
+     * @param className The name of the classroom to find. ex: "H-110".
+     * @return Returns the LatLng coordinate of the classroom. If there is no match, returns null.
+     */
     public LatLng searchByClassName(String className)
     {
         String currentRoom;
-        for (int i = 0; i < nodes.length; i++)
+        for (Node node : nodes)
         {
-            currentRoom = nodes[i].getRoom();
-            if (currentRoom.equals(className))  //we have found the room
+            currentRoom = node.getRoom();
+            if (currentRoom.equals(className))  // we have found the room
             {
-                return nodes[i].getElement();
+                return node.getElement();
             }
         }
         return null; //if we don't get a match we return null
     }
 
-    // returns an array of points corresponding to the shortest path from --> to
+    /** Breath first search algorithm implemented to search for a path between 2 nodes
+     * @param from Latitude and longitude coordinate of the start node.
+     * @param to  Latitude and longitude coordinate of the destination node.
+     * @return Returns an array of points corresponding to the shortest path from start to destination.
+     */
     public Object[] breathFirstSearch(LatLng from, LatLng to)
     {
         // resets the results for each search
         if (breathFirstSearchResults.getHead() != null)
             breathFirstSearchResults = new N_aryTree();
-        for (int i = 0; i < nodes.length; i++)
-        {
-            if (nodes[i] != null)
+        for (Node node: nodes) {
+            if (node != null)
             {
-                if (nodes[i].getType() > 0) // not a class node
-                    nodes[i].setTraversed(false);
+                if (node.getType() > 0) // not a class node
+                    node.setTraversed(false);
                 else
-                    nodes[i].setTraversed(true);
+                    node.setTraversed(true);
             }
         }
         // Both points exist
@@ -246,8 +297,8 @@ public class Graph
         if (id1 < 0 || id2 < 0)
             return null;
         // Start by adding the from point to tree
-        LinkedList<LatLng> currentPointsToCycle = new LinkedList<LatLng>(new LatLng(0,0));
-        LinkedList<LatLng> newCurrentPointsToCycle = new LinkedList<LatLng>(new LatLng(0,0));
+        LinkedList<LatLng> currentPointsToCycle = new LinkedList<>(new LatLng(0,0));
+        LinkedList<LatLng> newCurrentPointsToCycle = new LinkedList<>(new LatLng(0,0));
         N_aryTree.TreeNode currentTreeNode = breathFirstSearchResults.getHead();
         currentTreeNode.setElement(from);
         LinkedList.Node currentPoint = null;
@@ -305,7 +356,7 @@ public class Graph
                 }
             }
             currentPointsToCycle = newCurrentPointsToCycle;
-            newCurrentPointsToCycle = new LinkedList<LatLng>(new LatLng(0, 0));
+            newCurrentPointsToCycle = new LinkedList<>(new LatLng(0, 0));
             LinkedList.Node treeNodeChild = currentTreeNode.getChildren().getHead();
             for (int i = 0; i < currentTreeNode.getChildren().size(); i++)
             {
@@ -315,24 +366,21 @@ public class Graph
         return null;
     }
 
-    // reads from a node file to add nodes to a graph
+    /** Reads from a node file to add the nodes to the graph.
+     * @param contents A String array representing the contents of the file.
+     * @return Returns a graph using all of the coordinates that were listed in the file.
+     */
     public static Graph addNodesToGraph(String[] contents)
     {
-        int currentPos = 0;
-        String currentLine = null;
+        int currentPos;
+        String currentLine;
         String floorName = null;
-        Graph returnMe = null;
-        LatLng currentCoordinate = null;
-        String roomNumber = null;
-        String[] lineString = null;
-        int posOfHalfway = 0;
-        double x_coordinate = 0, y_coordinate = 0;
-        int nodeType = 0;  //0 is for a classroom node, 1 is for a hall node
+        Graph returnMe;
         int nmbClassNodes = 0;
         int nmbHallNodes = 0;
 
-        LinkedList<LatLng> coordinatesToInsert = new LinkedList<LatLng>(new LatLng(0,0));
-        LinkedList<String> namesToInsert = new LinkedList<String>("");
+        LinkedList<LatLng> coordinatesToInsert = new LinkedList<>(new LatLng(0,0));
+        LinkedList<String> namesToInsert = new LinkedList<>("");
 
         try
         {
@@ -345,10 +393,7 @@ public class Graph
                     throw new InputMismatchException("Expected a name but didn't find one");
                 floorName = (currentLine.substring(currentPos + 15));
 
-                i++; i++;
-                currentLine = contents[i];
-                currentPos = currentLine.indexOf("classrooms:");
-                i++;
+                i++; i++; i++;
                 currentLine = contents[i];
 
                 while (currentLine.charAt(currentLine.length() - 1) != '}')
@@ -392,7 +437,6 @@ public class Graph
                 namesToInsert.add("ELEVATOR");
 
                 i++;
-                currentLine = contents[i];
             }
         } catch (InputMismatchException e) {
             e.printStackTrace();
@@ -435,41 +479,50 @@ public class Graph
         return returnMe;
     }
 
+    /** Helper method for reading a classroom coordinate from the input file.
+     * @param currentLine String representing the current line of text that is being read.
+     * @return Returns a latitude and longitude coordinate representing the location of a classroom node.
+     */
     private static LatLng readClassCoordinate(String currentLine)
     {
         String[] lineString = currentLine.split(":");
-        String floorName = lineString[0].trim(); // ex: H-801
-
         int currentPos = lineString[1].indexOf("{");
         int posOfHalfway = lineString[1].indexOf(",");
         double x_coordinate = Double.parseDouble(lineString[1].substring(currentPos+1,posOfHalfway));
         double y_coordinate = Double.parseDouble(lineString[1].substring(posOfHalfway+2, lineString[1].length()-2));
-        LatLng currentCoordinate = new LatLng(x_coordinate, y_coordinate);
-        return currentCoordinate;
+        return new LatLng(x_coordinate, y_coordinate); //return currentCoordinate
     }
 
+    /** Helper method for reading a classroom name from the input file.
+     * @param currentLine String representing the current line of text that is being read.
+     * @return Returns a String representing the name of the classroom.
+     */
     private static String readClassName(String currentLine)
     {
         String[] lineString = currentLine.split(":");
-        String floorName = lineString[0].trim(); // ex: H-801
-
-        return floorName;
+        return lineString[0].trim(); // return floor name
     }
 
+    /**
+     * Helper method for reading a hallway coordinate from the input file.
+     * @param currentLine String representing the current line of text that is being read.
+     * @return Returns a latitude and longitude coordinate representing the location of the hallway node.
+     */
     private static LatLng readHallCoordinate(String currentLine)
     {
         int currentPos = currentLine.indexOf("{");
         int posOfHalfway = currentLine.indexOf(",");
         double x_coordinate = Double.parseDouble(currentLine.substring(currentPos+1,posOfHalfway));
         double y_coordinate = Double.parseDouble(currentLine.substring(posOfHalfway+2, currentLine.length()-2));
-        LatLng currentCoordinate = new LatLng(x_coordinate, y_coordinate);
-        return currentCoordinate;
+        return new LatLng(x_coordinate, y_coordinate);
     }
 
+    /** Method for adding edges between all nodes that are within a certain distance from eachother.
+     */
     public void addAdjacentNodes()
     {
         // we want to insert an edge between all of the nodes that are adjacent. i.e the nodes that are closest to eachother
-        float distance = 0;
+        float distance;
         for (int i = 0; i < nodes.length; i++)
         {
             Node currentNode = nodes[i];
@@ -482,7 +535,11 @@ public class Graph
         }
     }
 
-    // calculates the distance between 2 nodes in meters
+    /** Helper method for calculating the distance between 2 nodes.
+     * @param node1 latitude and longitude coordinate of the 1st node.
+     * @param node2 latitude and longitude coordinate of the 2nd node.
+     * @return Returns a float representing the distance between the 2 nodes.
+     */
     private float calculateNodeDistance(LatLng node1, LatLng node2)
     {
         float[] distance = new float[1];
