550e5f0a7d50e27e645f620bf6eeacdf0c6b75eb
Jay <jaychenblue@gmail.com>
2020-04-18 17:32:17 -0400

    [#255] Refactored the FileAccess package to respect the team's code style
    The team's code style can be found in the GitHub wiki; some commented out code was also removed

--- app/src/main/java/com/example/scoutconcordia/FileAccess/DES.java
+++ app/src/main/java/com/example/scoutconcordia/FileAccess/DES.java
@@ -21,30 +21,23 @@ import javax.crypto.spec.DESKeySpec;
 public class DES
 {
     private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
-
-    //private static KeyGenerator keygenerator;
-    //static {
-    //    try {
-    //        keygenerator = KeyGenerator.getInstance("DES");
-    //    } catch (NoSuchAlgorithmException e) {
-    //        e.printStackTrace();
-    //    }
-    //}
-    //final private static SecretKey myDesKey = keygenerator.generateKey();
-
     private static String desKey;
     private static byte[] keyBytes;
     private static SecretKeyFactory factory;
     private static SecretKey myDesKey;
 
-    DES() {
+    DES()
+    {
         this.desKey = "0123456789abcdef";
         this.keyBytes = hexToByte(this.desKey);
         {
-            try {
+            try
+            {
                 this.factory = SecretKeyFactory.getInstance("DES");
                 this.myDesKey = this.factory.generateSecret(new DESKeySpec(this.keyBytes));
-            } catch (NoSuchAlgorithmException | InvalidKeyException | InvalidKeySpecException e) {
+            }
+            catch (NoSuchAlgorithmException | InvalidKeyException | InvalidKeySpecException e)
+            {
                 e.printStackTrace();
             }
         }
@@ -83,43 +76,12 @@ public class DES
         return returnMe.toArray();
     }
 
-//    /** Method for encrypting a file. Requires an input stream and an output stream **/
-//    public void encryptFile(InputStream readFromMe, OutputStream writeToMe)
-//    {
-//        Scanner reader = null;
-//        PrintWriter writer = null;
-//        try
-//        {
-//            Cipher desCipher = Cipher.getInstance("DES");
-//            reader = new Scanner(readFromMe);
-//            writer = new PrintWriter(writeToMe);
-//
-//            // reads from the input file and outputs encrypted text to the output file
-//            while (reader.hasNextLine())
-//            {
-//                desCipher.init(Cipher.ENCRYPT_MODE, myDesKey);
-//                String encryptMe = reader.nextLine();
-//                byte[] textEncrypted = desCipher.doFinal(encryptMe.getBytes("UTF-8"));
-//                writer.println(bytesToHex(textEncrypted));
-//            }
-//        }
-//        catch(Exception e)
-//        {
-//            Log.println(Log.WARN, "decrypting", "An Exception occured");
-//        }
-//        finally
-//        {
-//            if (reader != null)
-//                reader.close();
-//            if (writer != null)
-//                writer.close();
-//        }
-//    }
-
     /** Helper method for converting an array of bytes to hex characters **/
-    private static String bytesToHex(byte[] bytes) {
+    private static String bytesToHex(byte[] bytes)
+    {
         char[] hexChars = new char[bytes.length * 2];
-        for (int j = 0; j < bytes.length; j++) {
+        for (int j = 0; j < bytes.length; j++)
+        {
             int v = bytes[j] & 0xFF;
             hexChars[j * 2] = HEX_ARRAY[v >>> 4];
             hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
@@ -128,10 +90,12 @@ public class DES
     }
 
     /** Helper method for converting hex characters to an array of bytes **/
-    private static byte[] hexToByte(String s) {
+    private static byte[] hexToByte(String s)
+    {
         int len = s.length();
         byte[] data = new byte[len / 2];
-        for (int i = 0; i < len; i += 2) {
+        for (int i = 0; i < len; i += 2)
+        {
             data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
                     + Character.digit(s.charAt(i+1), 16));
         }
